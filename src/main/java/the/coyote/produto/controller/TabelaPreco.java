279ae5e32bd9f0f0c1bbfa2a49bd820d0521037e
flavio luna ferreira <flaviolunaferreira@gmail.com>
2024-10-25 10:26:21 -0300

    escrevendo classes de controller

---
+++ src/main/java/the/coyote/produto/controller/TabelaPreco.java
@@ -0,0 +1,68 @@
+package the.coyote.produto.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import lombok.RequiredArgsConstructor;
+import the.coyote.produto.service.TabelaPrecoService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/tabelapreco")
+public class TabelaPreco {
+    
+    private TabelaPrecoService tabelaPrecoService;
+
+    
+    @GetMapping()
+    public ResponseEntity<?> getAll() {
+        try {
+            //TODO Implement Your Logic To Get Data From Service Layer Or Directly From Repository Layer
+            return new ResponseEntity<>("GetAll Results", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOne(@PathVariable Integer id) {
+        try {
+            //TODO Implement Your Logic To Get Data From Service Layer Or Directly From Repository Layer
+            return new ResponseEntity<>("GetOne Result", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @PostMapping()
+    public ResponseEntity<?> create(@RequestBody Dto dto) {
+        try {
+            //TODO Implement Your Logic To Save Data And Return Result Through ResponseEntity
+            return new ResponseEntity<>("Create Result", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @PutMapping()
+    public ResponseEntity<?> update(@RequestBody Dto dto) {
+        try {
+            //TODO Implement Your Logic To Update Data And Return Result Through ResponseEntity
+            return new ResponseEntity<>("Update Result", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> destroy(@PathVariable Integer id) {
+        try {
+            //TODO Implement Your Logic To Destroy Data And Return Result Through ResponseEntity
+            return new ResponseEntity<>("Destroy Result", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
---
+++ src/main/java/the/coyote/produto/service/EstoqueService.java
@@ -0,0 +1,5 @@
+package the.coyote.produto.service;
+
+public interface EstoqueService {
+
+}
